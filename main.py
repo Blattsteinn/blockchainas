20caaf31cd89ae851f308dc37b24a1e9cc9ea68c
Blattsteinn <arnas.lukosius@outlook.com>
2025-09-21 11:16:47 +0300

    simplified & improved unpredictibility

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 
-def hash_function(_array_of_bytes):
-    MOD = 2 ** 256 - 189  # Large prime for reduction
+def hash_function(_array_of_bits):
+    modulo = 2 ** 256 - 189
     calculated_number: int = 1
-    for _array_of_bits in _array_of_bytes:
-        bit_duo = []
-        temporary_sum: int = 1
 
-        for j in range(8):
-            if j < 6:
-                if _array_of_bits[j] == 0:
-                    temporary_sum *= 67429 ** (j+1)
-                if _array_of_bits[j] == 1:
-                    temporary_sum *= 91997 ** (j+1)
-            else:
-                bit_duo.append(_array_of_bits[j])
+    temporary_sum: int = 1
+    temporary_index: int = 0
+    bit_duo = []
+    print(_array_of_bits)
+    for i, _bit in enumerate(_array_of_bits):
+
+        if (i + 1) % 8 == 0:
+            calculated_number *= temporary_sum
+            calculated_number %= modulo
+
+            bit_duo = []
+            temporary_sum = 1
+            temporary_index = 0
 
 
-        if bit_duo == [0, 0]: temporary_sum *= 410079252992648349570025508981
-        if bit_duo == [0, 1]: temporary_sum *= 728730155706782142465312831289
-        if bit_duo == [1, 1]: temporary_sum *= 233246422214829721422450466691
-        if bit_duo == [1, 0]: temporary_sum *= 820249177872665494536846925429
+        if _array_of_bits[i] == 0:
+            temporary_sum *= 67429 ** (temporary_index+1)
+        else:
+            temporary_sum *= 91997 ** (temporary_index+1)
 
-        temporary_sum %= MOD
-        calculated_number *= temporary_sum
-        calculated_number %= MOD
+        if i >= 6 and (i % 6 == 0 or i % 6 == 1):
+            bit_duo.append(_array_of_bits[i])
+
+        if len(bit_duo) == 2:
+            if bit_duo == [0, 0]: temporary_sum *= 410079252992648349570025508981
+            if bit_duo == [0, 1]: temporary_sum *= 728730155706782142465312831289
+            if bit_duo == [1, 1]: temporary_sum *= 233246422214829721422450466691
+            if bit_duo == [1, 0]: temporary_sum *= 820249177872665494536846925429
+
+        temporary_sum %= modulo
+        print(temporary_sum)
 
     return calculated_number
 
@@ -33,19 +43,15 @@ if __name__ == "__main__":
         word_to_hash = input("Enter the word you want to hash: ")
         #word_to_hash = 'moo'
 
-        array_of_bytes = []
+        array_of_bits = []
 
         encoded_text = word_to_hash.encode('utf-8')
         for letter in encoded_text:
-            array_of_bits = []
             byte_form = format(letter, '08b')
             for bit in byte_form:
                 array_of_bits.append(int(bit))
-            array_of_bytes.append(array_of_bits)
 
 
-
-        number_received = hash_function(array_of_bytes)
-
+        number_received = hash_function(array_of_bits)
         hex_value = format(number_received, "x")
         print(hex_value)
